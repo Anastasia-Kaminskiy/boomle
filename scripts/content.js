0303059464220671ac740c803bad3abd2fcb68aa
Anastasia Kaminskiy <AnastasiaKaminskiy@gmail.com>
2026-09-21 23:10:59 +0300

    wip: kinda working styles :D

--- scripts/content.js
+++ scripts/content.js
@@ -9,26 +9,40 @@ function blurAIOverview(element) {
 
   var wrapper = document.createElement('div');
   wrapper.classList.add("wraapper");
+
   wrapper.appendChild(element.cloneNode(true)); 
   element.parentNode.replaceChild(wrapper, element);
 };
 
 const cssRules = `
-.wraapper {
-    background: red;
+  .wraapper {
+      max-height: 30vh;
+      overflow-y: auto;
+      overflow-x: hidden;
+      margin-top: -8px;
+      padding-top: 8px;
+      padding-left: 20px;
+      margin-left: -20px;
+      -ms-overflow-style: none;  
+    scrollbar-width: none;  
+  };
+  .container::-webkit-scrollbar { 
+    display: none;
 };
 `;
 
 const style = document.createElement('style');
 
 style.textContent = cssRules;
-style.id = "custom"
+style.id = "custom";
 
 document.head.append(style);
 
-const targetElement = document.getElementById("dEwkXc");
+
+// const possibleAIOverviewElementIDs = ["dEwkXc", "eKIzJc"]
+
+const aiOverviewBody = document.getElementById("m-x-content");
+const biggestParentOfAiOverview = aiOverviewBody.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement;
 
 
-blurAIOverview(targetElement);
-
-// V3qe9d
+blurAIOverview(biggestParentOfAiOverview);
